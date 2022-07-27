bb07d39066ec4eb25325d32bac8330eafcdec441
Ari Kurniawan <arikurnia2601@gmail.com>
2022-07-28 01:30:54 +0700

    menambahkan beberapa menu item.

--- app/src/main/java/com/rii/puzzlegame/MainActivity.java
+++ app/src/main/java/com/rii/puzzlegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.rii.puzzlegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // nampilin menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
     }
 }
